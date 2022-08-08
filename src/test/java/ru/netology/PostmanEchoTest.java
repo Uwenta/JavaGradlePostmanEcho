be481d4d53a2bd1ed2bac10170a6676987a7aa50
ElenaPonomareva <ponomareva@internet.ru>
2022-08-08 12:15:46 +0500

    first commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+
+
+    @Test
+    public void shouldPostRequest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("I love You")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("I love You"))
+        ;
+    }
+}
