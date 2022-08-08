eabc927e12dab7c9857e7dd39549f36112102cd2
ElenaPonomareva <ponomareva@internet.ru>
2022-08-08 12:46:38 +0500

    build failed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("I love You"))
+                .body("data", equalTo("I love me"))
         ;
     }
 }
